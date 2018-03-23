6e8a4fb4eef64bf782d42537622e0d1303987fb0
maxiongfei <18710839146@163.com>
2018-03-23 14:44:42 +0800

    edit

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -14,11 +14,26 @@ use think\Session;
  */
 class Base extends Controller
 {
+    protected static $redis = null;
+
     /**
      * 初始化
      * Base constructor.
      */
     public function __construct()
     {
+        Session::set('user_id', 2);
+        if (is_null(self::$redis)) {
+            $server = array(
+                'host'     => '127.0.0.1',
+                'port'     => 6379,
+                'database' => 0
+            );
+            self::$redis = new Client($server);
+        } else {
+            return self::$redis;
+        }
+
+
     }
 }
